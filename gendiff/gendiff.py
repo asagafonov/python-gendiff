874e0cefa089704a8b0f67fe6d06b0ac0cea0b88
asagafonov <a.s.agafonov@yandex.ru>
2021-07-09 00:27:03 +0300

    add diff generator

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,9 +1,65 @@
-import argparse
+import pathlib
+from gendiff.parser import parse
+from gendiff.formatters.diff import format_diff
 
 
-parser = argparse.ArgumentParser(description='Generate diff')
-parser.add_argument('first_file', metavar='first_file', type=str)
-parser.add_argument('second_file', metavar='second_file', type=str)
-parser.add_argument('-f', '--format', help='set format of output')
+def prepare_diff(obj1, obj2):
+    keys1 = set(obj1.keys())
+    keys2 = set(obj2.keys())
+    common_keys = sorted(keys1.union(keys2))
 
-args = parser.parse_args()
+    result = []
+
+    for key in common_keys:
+        if key not in obj1:
+            result.append({
+                'name': key,
+                'status': 'added',
+                'value': obj2[key]
+            })
+        elif key not in obj2:
+            result.append({
+                'name': key,
+                'status': 'deleted',
+                'value': obj1[key]
+            })
+        elif type(obj1[key]) == 'object' and type(obj2[key]) == 'object':
+            result.append({
+                'name': key,
+                'status': 'unknown',
+                'children': prepare_diff(obj1[key], obj2[key])
+            })
+        elif obj1[key] == obj2[key]:
+            result.append({
+                'name': key,
+                'status': 'unmodified',
+                'value': obj1[key]
+            })
+        else:
+            result.append({
+                'name': key,
+                'status': 'modified',
+                'old_value': obj1[key],
+                'new_value': obj2[key]
+            })
+
+    return result
+
+
+def gendiff(filepath1, filepath2, format):
+    full_path1 = pathlib.Path(filepath1).resolve()
+    full_path2 = pathlib.Path(filepath2).resolve()
+
+    def get_extension(filepath):
+        return pathlib.Path(filepath).suffix
+
+    file1_ext = get_extension(filepath1)
+    file2_ext = get_extension(filepath2)
+
+    file1 = parse(full_path1, file1_ext)
+    file2 = parse(full_path2, file2_ext)
+
+    diff = prepare_diff(file1, file2)
+    output = format_diff(diff)
+
+    return output
