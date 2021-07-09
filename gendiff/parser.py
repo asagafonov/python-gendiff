14f8f876dd6612ac1709c044bba8c0228fbe5e68
asagafonov <a.s.agafonov@yandex.ru>
2021-07-09 17:19:16 +0300

    remove ini format option

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,6 +1,5 @@
 import json
 import yaml
-import ini
 
 
 def parse(filepath, ext):
@@ -8,7 +7,5 @@ def parse(filepath, ext):
         return json.load(open(filepath))
     elif ext == '.yml' or ext == '.yaml':
         return yaml.safe_load(open(filepath))
-    elif ext == '.ini':
-        return ini.load(open(filepath))
     else:
         raise Exception('{} file format not supported'.format(ext))
