f12bb36a7d1903558c77701e9d378cdf98b9c1f5
asagafonov <a.s.agafonov@yandex.ru>
2021-07-13 14:25:56 +0300

    add tests, fixtures rename directory

---
+++ tests/test_gendiff.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from gendiff.gendiff import generate
+
+
+stylish_path = './tests/fixtures/expectedStylish.txt'
+plain_path = './tests/fixtures/expectedPlain.txt'
+json_path = './tests/fixtures/expectedJSON.txt'
+
+
+expectedStylish = Path(stylish_path).read_text().strip()
+expectedPlain = Path(plain_path).read_text().strip()
+expectedJSON = Path(json_path).read_text().strip()
+
+
+def test_generate_from_json():
+    path1 = './tests/fixtures/before.json'
+    path2 = './tests/fixtures/after.json'
+
+    # assert generate(path1, path2, 'stylish') == expectedStylish
+    assert generate(path1, path2, 'plain') == expectedPlain
+    assert generate(path1, path2, 'json') == expectedJSON
+
+
+def test_generate_from_yaml():
+    path1 = './tests/fixtures/before.yml'
+    path2 = './tests/fixtures/after.yml'
+
+    # assert generate(path1, path2, 'stylish') == expectedStylish
+    assert generate(path1, path2, 'plain') == expectedPlain
+    assert generate(path1, path2, 'json') == expectedJSON
