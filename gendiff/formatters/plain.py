67a2ed3efe9bda9a00185ac3f943142200fb5a74
asagafonov <a.s.agafonov@yandex.ru>
2021-07-13 14:26:26 +0300

    upd plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,11 +1,4 @@
-def flatten(collection):
-    result = []
-    for el in collection:
-        if isinstance(el, list):
-            result.extend(flatten(el))
-        else:
-            result.append(el)
-    return result
+from gendiff.utils.flatten import flatten
 
 
 def stringify(val):
@@ -18,22 +11,33 @@ def stringify(val):
     return '\'{}\''.format(val)
 
 
-def add_plain_formatting(diff):
+def add_plain_formatting(diff): # noqa C901
     def iter(data, path):
         result = []
         for item in data:
             new_path = '{}.{}'.format(path, item['name'])
 
             if item['status'] == 'added':
-                result.append('Property {} was added with value: {}'.format(new_path[1:], stringify(item['value'])))
+                result.append(
+                    'Property \'{}\' was added with value: {}'.format(
+                        new_path[1:],
+                        stringify(item['value'])
+                    ))
             elif item['status'] == 'deleted':
-                result.append('Property {} was deleted'.format(new_path[1:]))
+                result.append(
+                    'Property \'{}\' was removed'.format(new_path[1:])
+                )
             elif item['status'] == 'unknown':
                 result.append(iter(item['children'], new_path))
             elif item['status'] == 'unmodified':
                 pass
             elif item['status'] == 'modified':
-                result.append('Property {} was updated from {} to {}'.format(new_path[1:], stringify(item['old_value']), stringify(item['new_value'])))
+                result.append(
+                    'Property \'{}\' was updated from {} to {}'.format(
+                        new_path[1:],
+                        stringify(item['old_value']),
+                        stringify(item['new_value'])
+                    ))
             else:
                 raise Exception('Unknown status: {}'.format(item['status']))
 
